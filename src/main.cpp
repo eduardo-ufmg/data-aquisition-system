755a57403372ada21ef5639041157dcda0b30b98
Eduardo Henrique Basilio de Carvalho <eduardohbc@ufmg.br>
2024-06-21 14:35:27 +0000

    store_sensor_data implementada

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,27 @@
 #include <ctime>
 #include <iomanip>
 #include <sstream>
+#include <regex>
+#include <fstream>
 
-const std::string field_delimiter = "|";
-const std::string sensor_first_field = "LOG";
-const std::string client_first_field = "GET";
+const std::string msg_delimiter      = "\r\n";
+const std::string field_delimiter    =    "|";
+const std::string sensor_first_field =  "LOG";
+const std::string client_first_field =  "GET";
+
+const std::regex client_pattern("[\x00-\x7F]{1,32}\|[0-9]+\r\n");
+const std::regex sensor_pattern("[\x00-\x7F]{1,32}\|[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+\|[0-9]+.?[0-9]+\r\n");
 
 using boost::asio::ip::tcp;
 
-std::time_t string_to_time_t(const std::string& time_string) {
-    std::tm tm = {};
-    std::istringstream ss(time_string);
-    ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%S");
-    return std::mktime(&tm);
-}
+struct LogRecord {
+  std::time_t timestamp;
+  double value;
+};
 
-std::string time_t_to_string(std::time_t time) {
-    std::tm* tm = std::localtime(&time);
-    std::ostringstream ss;
-    ss << std::put_time(tm, "%Y-%m-%dT%H:%M:%S");
-    return ss.str();
-}
+std::time_t string_to_time_t(const std::string& time_string);
+std::string time_t_to_string(std::time_t time);
+std::string extract_msg_field(std::string& message, const std::string& delimiter);
 
 class session : public std::enable_shared_from_this<session> {
 public:
@@ -39,67 +40,84 @@ public:
 private:
   void read_message() {
     auto self(shared_from_this());
-    boost::asio::async_read_until(socket_, buffer_, "\r\n",
-        [this, self](boost::system::error_code ec, std::size_t length) {
-          if (!ec) {
-            std::istream is(&buffer_);
-            std::string message(std::istreambuf_iterator<char>(is), {});
-            std::cout << "Received: " << message << std::endl;
+    boost::asio::async_read_until(socket_, read_buffer_, msg_delimiter,
+      [this, self](boost::system::error_code ec, std::size_t length) {
+        if(!ec) {
+          std::istream is(&read_buffer_);
+          std::string message(std::istreambuf_iterator<char>(is), {});
+          std::cout << "Received: " << message << std::endl;
 
-            std::size_t pos = message.find(field_delimiter);
+          bool is_valid = false;
 
-            if (pos == std::string::npos) {
-              std::cerr << "Invalid message format" << std::endl;
-              return;
+          std::string first_field = extract_msg_field(message, field_delimiter);
+
+          if(first_field == sensor_first_field) {
+            
+            if(std::regex_match(message, sensor_pattern)) {
+              is_valid = true;
+              store_sensor_data(message);
             }
 
-            std::string first_field = message.substr(0, pos);
-            message = message.substr(pos + 1);
-
-            if (first_field == sensor_first_field) {
-              store_sensor_data(message);
-            } else if (first_field == client_first_field) {
+          } else if (first_field == client_first_field) {
+            
+            if(std::regex_match(message, client_pattern)) {
+              is_valid = true;
               answer_client(message);
-            } else {
-              std::cerr << "Invalid message format" << std::endl;
-              return;
             }
 
           }
-        });
+
+          read_message();
+
+          if(!is_valid)
+            goto invalid_message_format;
+
+          return;
+
+          invalid_message_format:
+          std::cerr << "Invalid message format" << std::endl;
+
+        }
+      });
   }
 
   void answer_client(std::string& message) {
 
-    std::size_t pos = message.find(field_delimiter);
+    std::string sensor_id = extract_msg_field(message, field_delimiter);
 
-    if (pos == std::string::npos) {
-      std::cerr << "Invalid message format" << std::endl;
-      return;
-    }
-
-    std::string sensor_id = message.substr(0, pos);
-    message = message.substr(pos + 1);
-
-    
+    std::string sample_quantity = extract_msg_field(message, msg_delimiter);
 
     auto self(shared_from_this());
-    boost::asio::async_write(socket_, boost::asio::buffer("message"),
-        [this, self](boost::system::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            read_message();
-          }
-        });
+    boost::asio::async_write(socket_, write_buffer_,
+      [this, self](boost::system::error_code ec, std::size_t length) {});
   }
 
   void store_sensor_data(std::string& message) {
 
+    std::string sensor_id = extract_msg_field(message, field_delimiter);
+
+    std::string timestamp = extract_msg_field(message, field_delimiter);
+
+    std::string value = extract_msg_field(message, msg_delimiter);
+
+    LogRecord record = {string_to_time_t(timestamp), std::stod(value)};
+
+    std::fstream file(sensor_id + ".dat", std::ios::out | std::fstream::binary | std::ios::app);
+
+    if(!file.is_open()) {
+      std::cerr << "Error opening file" << std::endl;
+      return;
+    }
+
+    file.write((char*)&record, sizeof(LogRecord));
+
+    file.close();
+
   }
 
   tcp::socket socket_;
-  boost::asio::streambuf buffer_;
+  boost::asio::streambuf read_buffer_;
+  boost::asio::streambuf write_buffer_;
 };
 
 class server {
@@ -113,7 +131,7 @@ private:
   void accept() {
     acceptor_.async_accept(
         [this](boost::system::error_code ec, tcp::socket socket) {
-          if (!ec) {
+          if(!ec) {
             std::make_shared<session>(std::move(socket))->start();
           }
 
@@ -125,16 +143,42 @@ private:
 };
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    std::cerr << "Usage: chat_server <port>\n";
-    return 1;
+
+  int port;
+
+  if(argc == 2) {
+    port = std::atoi(argv[1]);
+  } else {
+    std::cerr << "Usage: chat_server <port>. 9000 will be used\r\n";
+    port = 9000;
   }
 
   boost::asio::io_context io_context;
 
-  server s(io_context, std::atoi(argv[1]));
+  server s(io_context, port);
 
   io_context.run();
 
   return 0;
+}
+
+std::time_t string_to_time_t(const std::string& time_string) {
+  std::tm tm = {};
+  std::istringstream ss(time_string);
+  ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%S");
+  return std::mktime(&tm);
+}
+
+std::string time_t_to_string(std::time_t time) {
+  std::tm* tm = std::localtime(&time);
+  std::ostringstream ss;
+  ss << std::put_time(tm, "%Y-%m-%dT%H:%M:%S");
+  return ss.str();
+}
+
+std::string extract_msg_field(std::string& message, const std::string& delimiter) {
+  std::size_t pos = message.find(delimiter);
+  std::string field = message.substr(0, pos);
+  message = message.substr(pos + 1);
+  return field;
 }
